992ccaa29af9859bcb5543119eb0a309386565ff
Neupokoev Semyon <semyon_neupokoev@mail.ru>
2025-10-06 19:20:31 +0300

    Task0

--- Tyuiu.NeupokoevSV.Sprint3.Task0.V16/Program.cs
+++ Tyuiu.NeupokoevSV.Sprint3.Task0.V16/Program.cs
@@ -3,6 +3,17 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DataService ds = new DataService();
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        double x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+        int z = Convert.ToInt32(Console.ReadLine());
+        double res = ds.GetSumSeries(x, y, z);
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(res);
     }
 }
