a8d5168a382f7f0dd622b061aa865d2bd1284e55
Neupokoev Semyon <semyon_neupokoev@mail.ru>
2025-10-08 21:20:47 +0300

    Task6

---
+++ Tyuiu.NeupokoevSV.Sprint3.Task6.V5/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.NeupokoevSV.Sprint3.Task6.V5.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int startvalue1 = 15;
+        int stopValue1 = 22;
+        double res = ds.GetSumTheDivisors(startvalue1, stopValue1);
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(res);
+    }
+}
