275845440ff23b7ae9bcba09d34223c1ab645f17
Neupokoev Semyon <semyon_neupokoev@mail.ru>
2025-10-08 18:18:49 +0300

    Task5

--- Tyuiu.NeupokoevSV.Sprint3.Task5.V23/Program.cs
+++ Tyuiu.NeupokoevSV.Sprint3.Task5.V23/Program.cs
@@ -3,6 +3,19 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DataService ds = new DataService();
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int x = 2;
+        int startvalue1 = 1;
+        int startvalue2 = 1;
+        int stopValue1 = 3;
+        int stopvalue2 = 10;
+        double res = ds.GetSumSumSeries(x, startvalue1, stopValue1, startvalue2, stopvalue2);
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(res);
     }
 }
