b0edb40e2d8508980bcc5a00a5a90782e5225564
Neupokoev Semyon <semyon_neupokoev@mail.ru>
2025-10-08 21:21:18 +0300

    Task6

--- Tyuiu.NeupokoevSV.Sprint3.Task6.V5.Lib/DataService.cs
+++ Tyuiu.NeupokoevSV.Sprint3.Task6.V5.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.NeupokoevSV.Sprint3.Task6.V5.Lib
 {
-    public class DataService : ISprint3Task6V0
+    public class DataService : ISprint3Task6V5
     {
         public int GetSumTheDivisors(int startValue, int stopValue)
         {
