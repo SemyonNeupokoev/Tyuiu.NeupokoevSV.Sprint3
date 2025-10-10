ce910baef00854851ebc0447807d28a8b5bc134d
Neupokoev Semyon <semyon_neupokoev@mail.ru>
2025-10-10 09:49:47 +0300

    Task7

---
+++ Tyuiu.NeupokoevSV.Sprint3.Task7.V11.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.NeupokoevSV.Sprint3.Task7.V11.Lib;
+namespace Tyuiu.NeupokoevSV.Sprint3.Task7.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+            int len = stopValue - startValue + 1;
+            double[] valuewaitArray = new double[len];
+            valuewaitArray[0] = 20.36;
+            valuewaitArray[1] = 15.42;
+            valuewaitArray[2] = 10.99;
+            valuewaitArray[3] = 7.43;
+            valuewaitArray[4] = 4.33;
+            valuewaitArray[5] = 1.00;
+            valuewaitArray[6] = 0.00;
+            valuewaitArray[7] = -8.87;
+            valuewaitArray[8] = -13.03;
+            valuewaitArray[9] = -16.53;
+            valuewaitArray[10] = -19.60;
+            double[] res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(valuewaitArray, res);
+        }
+    }
+}
---
+++ Tyuiu.NeupokoevSV.Sprint3.Task7.V11/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.NeupokoevSV.Sprint3.Task7.V11.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int startvalue1 = -5;
+        int stopValue1 = 5;
+        int len = ds.GetMassFunction(startvalue1, stopValue1).Length;
+        double[] valueArray;
+        valueArray = new double[len];
+        valueArray = ds.GetMassFunction(startvalue1, stopValue1);
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("+--------+--------+");
+        Console.WriteLine("|    X   |    F(x)|");
+        Console.WriteLine("+--------+--------+");
+
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.WriteLine("|{0, 5:d}          | {1, 5:f2}", startvalue1, valueArray[i]);
+            startvalue1++;
+        }
+        Console.WriteLine("");
+    }
+}
